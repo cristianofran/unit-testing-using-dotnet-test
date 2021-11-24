6e2a314f1776b54a70de616912219f6fc0d0fb29
Cristiano Silva <cristiano.silva@agenciaopen.com>
2021-11-24 18:13:58 -0300

    Criação do métricas de cobertura de código.

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -5,51 +5,25 @@ namespace PrimeService.Tests
     public class PrimeServiceIsPrimeShould
     {
         private Prime.Services.PrimeService _primeService;
+        public PrimeServiceIsPrimeShould() => _primeService = new Prime.Services.PrimeService();
 
-        public PrimeServiceIsPrimeShould()
-        {
-            _primeService = new Prime.Services.PrimeService();
-        }
+        [
+            Theory,
+            InlineData(-1), InlineData(0), InlineData(1)
+        ]
+        public void IsPrime_InputIs1_ReturnFalse(int value) => Assert.False(_primeService.IsPrime(value), $"{value} should not be prime.");
 
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(1)]
-        public void IsPrime_InputIs1_ReturnFalse(int value)
-        {
-            // Act
-            var result = _primeService.IsPrime(value);
-            
-            // Assert
-            Assert.False(result, $"{value} should not be prime.");
-        }
+        [
+            Theory,
+            InlineData(2), InlineData(3), InlineData(5), InlineData(7)
+        ]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value) => Assert.True(_primeService.IsPrime(value), $"{value} should be prime.");
 
-        [Theory]
-        [InlineData(2)]
-        [InlineData(3)]
-        [InlineData(5)]
-        [InlineData(7)]
-        public void IsPrime_PrimesLessThan10_ReturnTrue(int value)
-        {
-            // Act
-            var result = _primeService.IsPrime(value);
 
-            // Assert
-            Assert.True(result, $"{value} should be prime.");
-        }
-
-        [Theory]
-        [InlineData(4)]
-        [InlineData(6)]
-        [InlineData(8)]
-        [InlineData(9)]
-        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value)
-        {
-            // Act
-            var result = _primeService.IsPrime(value);
-
-            // Assert
-            Assert.False(result, $"{value} should not be prime");
-        }
+        [
+            Theory,
+            InlineData(4), InlineData(6), InlineData(8), InlineData(9)
+        ]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value) => Assert.False(_primeService.IsPrime(value), $"{value} should not be prime");
     }
 }
